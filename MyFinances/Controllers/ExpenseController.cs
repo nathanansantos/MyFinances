1c6dcbb3ec74b5b7a09993c2675816c88c140788
Nathan Santos <nathanansantos@gmail.com>
2024-09-19 22:12:50 -0300

    Correção de bugs

--- MyFinances/Controllers/ExpenseController.cs
+++ MyFinances/Controllers/ExpenseController.cs
@@ -1,9 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using MyFinances.Models;
 using MyFinances.DAL;
 
